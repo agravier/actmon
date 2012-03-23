7904ff384264de6b6e2031f3b602e13f8bcbfd46
Alexandre Gravier <al.gravier@gmail.com>
2012-03-24 01:52:26 +0800

    Add source files and README.

---
+++ actmon.c
@@ -0,0 +1,63 @@
+/*
+This file is part of actmon.
+
+actmon is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+actmon is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with actmon.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Dependencies: X11 libxss python2.7
+// Compiling: python2 setup.py build
+// Packaging: python2 setup.py sdist
+// Installing: python2 setup.py install
+
+#include <python2.7/Python.h>
+
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/extensions/scrnsaver.h>
+
+
+static PyObject * actmon_get_idle_time(PyObject *self, PyObject *args) {
+  /* Returns idle time for the present display in milliseconds */
+  time_t idle_time;
+  static XScreenSaverInfo *mit_info;
+  Display *display;
+  int screen;
+  mit_info = XScreenSaverAllocInfo();
+  if((display=XOpenDisplay(NULL)) == NULL) { 
+    
+    return NULL; 
+  }
+  screen = DefaultScreen(display);
+  XScreenSaverQueryInfo(display, RootWindow(display,screen), mit_info);
+  idle_time = (mit_info->idle);
+  XFree(mit_info);
+  XCloseDisplay(display); 
+  return Py_BuildValue("k", (unsigned long)idle_time); // k stands for unsigned long
+}
+
+static PyMethodDef actmon_methods[] = {
+    {"get_idle_time",  actmon_get_idle_time, METH_NOARGS,
+     "Returns idle time for the present display in milliseconds."},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+PyMODINIT_FUNC
+initactmon(void)
+{
+    (void)Py_InitModule("actmon", actmon_methods);
+}
---
+++ setup.py
@@ -0,0 +1,56 @@
+# This file is part of actmon.
+
+# actmon is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# actmon is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with actmon.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from distutils.core import setup, Extension
+
+
+module = Extension('actmon',
+                   define_macros = [('MAJOR_VERSION', '0'),
+                                    ('MINOR_VERSION', '1')],
+                   include_dirs = ['/usr/include'],
+                   libraries = ['X11', 'Xext', 'Xss'],
+                   library_dirs = ['/usr/lib'],
+                   sources = ['actmon.c'])
+
+
+# patch distutils if it can't cope with the "classifiers" or
+# "download_url" keywords
+from sys import version
+if version < '2.2.3':
+    from distutils.dist import DistributionMetadata
+    DistributionMetadata.classifiers = None
+    DistributionMetadata.download_url = None
+
+setup(name = 'actmon',
+      version = '0.1b1',
+      author = 'Alexandre Gravier',
+      author_email = 'al.gravier@gmail.com',
+      url = 'http://agravier.github.com/actmon',
+      description = 'This package provides a user idle timer for X11.',
+      long_description = 'This package provides a user idle timer for X11.',
+      license = 'GNU General Public License (GPL) v3 and later',
+      platforms = ['X11'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: X11',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: C',
+        'Topic :: Software Development :: System Administration'],
+      ext_modules = [module])
